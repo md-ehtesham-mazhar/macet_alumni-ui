5bc08a232a073844566102681397c3d43846665e
MohammadSahil018 <sahilrizyi@gmail.com>
2025-04-10 12:17:35 +0530

    add frontend

--- CHAT-APPLICATION/backend/src/controllers/message.controller.js
+++ CHAT-APPLICATION/backend/src/controllers/message.controller.js
@@ -56,10 +56,10 @@ import User from "../models/user.model.js";
  
      await newMessage.save();
  
-     const receiverSocketId = getReceiverSocketId(receiverId);
-     if (receiverSocketId) {
-       io.to(receiverSocketId).emit("newMessage", newMessage);
-     }
+    //  const receiverSocketId = getReceiverSocketId(receiverId);
+    //  if (receiverSocketId) {
+    //    io.to(receiverSocketId).emit("newMessage", newMessage);
+    //  }
  
      res.status(201).json(newMessage);
    } catch (error) {
--- CHAT-APPLICATION/backend/src/index.js
+++ CHAT-APPLICATION/backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv, { config } from "dotenv";
 import cookieParser from "cookie-parser"
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -12,8 +13,16 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
